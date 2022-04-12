0a46f3fab676a99e7d2536c29b1fe8fe13968323
GuidoDalonzo <guido.dalonzo@hotmail.com>
2022-04-12 15:18:25 -0300

    Cart update

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,105 @@
-import { useCartContext } from "../../context/CartContext"
+import { Button } from '@material-ui/core';
+import React, { useContext, useState } from 'react'
+import '../Cart/Cart.css';
+import {CartContext} from '../../context/CartContext'
+import CartItem from '../Item/Item';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import {firestore} from "./firebase";
+import firebase from 'firebase/app';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
 function Cart() {
 
-  const { cartList, removeCart } = useCartContext()
-  return (
-    <div>
-      {cartList.map(prod => <li key={prod.id} > nombre: {prod.name} - cantidad: {prod.cantidad} </li>)}
-      <button className="btn btn-outline-warning" onClick={removeCart}>Vaciar Carrito</button>
-    </div>
-  )
+    const { cart, clearCart, total } = useContext(CartContext)
+
+    const [ openPay, setOpenPay ] = useState(false)
+
+    const [ name, setName ] = useState("")
+    const [ phone, setPhone ] = useState("")
+    const [ email, setEmail ] = useState("")
+
+    const classes = useStyles();
+
+
+    function submitOrder(){
+
+        
+        const db = firestore
+        const orders = db.collection('orders')
+
+        const order = {
+            buyer: { name: name, phone: phone, email: email},
+            items: cart,
+            date: firebase.firestore.Timestamp.fromDate(new Date()),
+            total: total,
+        }
+        orders.add(order)
+        .then(({ id }) => alert("Anotá el id de tu compra " + id))
+        .catch((error) => console.log(error))
+    }
+
+
+    return (
+        <div className="cart">
+            { cart.length > 0 ? (
+            <h1>A Comprar </h1>
+            )    
+            :
+            ( <>
+            <h1>Todavia no has elegido nada</h1>
+                <Link to={"/"}>
+                <Button onClick={clearCart} variant="contained" color="primary">
+                    Ir a la seccion productos
+                </Button>
+            </Link>
+            </>)}
+
+            <div className="cartItems">
+                { cart.length > 0 && cart.map( product => <CartItem key={product.id} 
+                id={product.id} name={product.name} image={product.image} price={product.price} 
+                amount={product.amount} />)}
+            </div>
+
+
+            { cart.length > 0 &&
+            <>
+            <h2>${total}</h2>
+            <div className="cartItems__buttons">
+                <Button onClick={clearCart} variant="contained" color="primary">
+                    Eliminar todos los articulos del carrito 
+                </Button>
+                <Button onClick={() => {setOpenPay(true)}} variant="contained" color="primary">
+                    Ir a caja 
+                </Button>
+            </div>
+            </>}
+
+            { openPay && 
+            <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="standard-basic" label="Name" value={name} 
+            onChange={(e) => setName(e.target.value)} />
+            <TextField id="standard-basic" label="Telephone" value={phone} 
+            onChange={(e) => setPhone(e.target.value)} />
+            <TextField id="standard-basic" label="Email" value={email} 
+            onChange={(e) => setEmail(e.target.value)} />
+            <Button onClick={submitOrder} variant="contained" color="primary">
+                    Si
+                </Button>
+          </form>}
+
+
+        </div>
+    )
 }
 
 export default Cart
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,35 +1,96 @@
-import { createContext, useContext, useState } from "react";
+import React, { useState, useEffect } from 'react'
 
 
-const CartContext = createContext([])
+export const CartContext = React.createContext();
 
-export const useCartContext = () => useContext(CartContext)
+function CartProvider( { children }) {
+
+    const [ cart, setCart ] = useState([])
+    const [ quantity, setQuantity ] = useState(0)
+    const [ total, setTotal ] = useState()
 
 
-function CartContextProvider({children}) { //componente
-    const [cartList, setCartList] = useState([])
+    useEffect(() => {
+        var t = 0
+        const totals = cart.map( p => p.price * p.amount)
+        totals.map( p => t = t + p)
+        setTotal(t)
+        const cartQuantity = cart.length
+        setQuantity(cartQuantity)
+    }, [cart])
 
-    const addToCart = (item) => {
-        setCartList([
-            ...cartList,
-            item
-        ])
+
+    function isInCart(id){
+        const item = cart.find(p => p.id === id)
+        if (item === undefined){
+            return false
+        }
+        else {
+            return true
+        }
     }
 
-    const removeCart = () => {
-        setCartList([])
+    function addToCart(product, counter, id) {
+         
+       
+        if (isInCart(id)){             
+            const oldProduct = cart.find(p => p.id === id)            
+            const newQuantity = oldProduct.amount + counter                      
+            const newProduct = { id: oldProduct.id, name: oldProduct.name, image: oldProduct.image, price: oldProduct.price, amount: newQuantity}          
+            const cartWithoutOld = cart.filter(product => product.id =! id)           
+            const cartWithNew = [...cartWithoutOld, newProduct]          
+            setCart(cartWithNew)            
+        } else {
+
+            const newItem = { id: product.id, name: product.name, image: product.image, price: product.price, amount: counter }
+            setCart([...cart, newItem]) 
+        }
     }
 
-    
+    function eliminateFromCart(id){
+
+        const newCart = cart.filter(product => product.id !== id)
+
+        setCart(newCart)
+    }
+
+    function clearCart(){
+
+        setCart([])
+        setQuantity(0)
+    }
+
+
     return (
-        <CartContext.Provider value={{
-            cartList,
-            addToCart,
-            removeCart
-        }}>
-            {children}
-        </CartContext.Provider>
+     
+            <CartContext.Provider value ={{ cart, quantity, total, addToCart, eliminateFromCart, clearCart }}>
+                { children }
+            </CartContext.Provider>
     )
 }
 
-export default CartContextProvider
+export default CartProvider;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
